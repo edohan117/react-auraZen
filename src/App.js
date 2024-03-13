8f2900f8f6b02d066d22c533d571e6070089fa2c
edohan <edohan0117@gmail.com>
2024-03-13 12:18:44 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer'
+
+const customer = [{
+  'id' : 1,
+  'image' : 'https://picsum.photos/id/1/200/150',
+  'name' : '홍길동',
+  'birthday' : '950117',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : 'https://picsum.photos/id/2/200/150',
+  'name' : '백재근',
+  'birthday' : '950117',
+  'gender' : '남자',
+  'job' : '백수'
+},
+{
+  'id' : 3,
+  'image' : 'https://picsum.photos/id/3/200/150',
+  'name' : '이도한',
+  'birthday' : '950117',
+  'gender' : '남자',
+  'job' : '개발자'
+}]
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt='logo'/>
-      <h2>Let's develop management system!</h2>
+    <div>
+      { customer.map(c => {return( <Customer key =  {c.id} id = {c.id} name = {c.name} image = {c.image} birthday = {c.birthday} gender = {c.gender} job = {c.job} /> )})}
     </div>
   );
 }
